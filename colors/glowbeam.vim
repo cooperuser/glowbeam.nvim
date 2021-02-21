0c37aea9aab31462c9cbbbd6b34bd30e1bf4706d
Cooper b. Anderson <andersc7@rose-hulman.edu>
2021-02-21 04:39:39 -0500

    Switch to using lush

---
+++ colors/glowbeam.vim
@@ -0,0 +1,141 @@
+"Theme built with Lush.nvim, exported at Sun 21 Feb 2021 04:36:17 AM EST
+highlight Hint guifg=#FDB4DB guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight! link NormalFloat Normal
+highlight TabLineFill guifg=NONE guibg=#1D2025 guisp=NONE gui=NONE blend=NONE
+highlight Normal guifg=#BAC1CE guibg=#141414 guisp=NONE gui=NONE blend=NONE
+highlight Ignore guifg=#E6E6E6 guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight PmenuSel guifg=#1D2025 guibg=#52B0EF guisp=NONE gui=NONE blend=NONE
+highlight Constant guifg=#97BE64 guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight StatusLine guifg=#B0B0B0 guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight LspReferenceText guifg=#141414 guibg=#FF6B6B guisp=NONE gui=NONE blend=NONE
+highlight ColorColumn guifg=NONE guibg=#1D2025 guisp=NONE gui=NONE blend=NONE
+highlight LspReferenceWrite guifg=#141414 guibg=#FF6B6B guisp=NONE gui=NONE blend=NONE
+highlight StatusLineNC guifg=#72787E guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight String guifg=#97BE64 guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight Keyword guifg=#C678DD guibg=NONE guisp=NONE gui=italic blend=NONE
+highlight Number guifg=#DA8549 guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight Info guifg=#52B0EF guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight DiffDelete guifg=NONE guibg=#494142 guisp=NONE gui=NONE blend=NONE
+highlight EndOfBuffer guifg=#3F444A guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight CursorLine guifg=NONE guibg=#23272F guisp=NONE gui=NONE blend=NONE
+highlight VertSplit guifg=#3F444A guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight! link CursorColumn CursorLine
+highlight Bold guifg=NONE guibg=NONE guisp=NONE gui=bold blend=NONE
+highlight Search guifg=NONE guibg=#342E0F guisp=NONE gui=NONE blend=NONE
+highlight! link NonText Whitespace
+highlight NvimTreeFolderIcon guifg=#72787E guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight Success guifg=#97BE64 guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight Function guifg=#52B0EF guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight TSParameter guifg=#4CB5BD guibg=NONE guisp=NONE gui=italic blend=NONE
+highlight Title guifg=#ECBE79 guibg=NONE guisp=NONE gui=bold blend=NONE
+highlight NvimTreeGitStaged guifg=#97BE64 guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight NvimTreeGitDirty guifg=#52B0EF guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight NvimTreeGitNew guifg=#FF6B6B guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight Conceal guifg=NONE guibg=#3F444A guisp=NONE gui=NONE blend=NONE
+highlight Cursor guifg=NONE guibg=#94E453 guisp=NONE gui=NONE blend=NONE
+highlight! link lCursor Cursor
+highlight CursorIM guifg=NONE guibg=#85ADFF guisp=NONE gui=NONE blend=NONE
+highlight Folded guifg=#F8E61B guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight FoldColumn guifg=#F8E61B guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight SignColumn guifg=NONE guibg=#141414 guisp=NONE gui=NONE blend=NONE
+highlight LineNr guifg=#3F444A guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight CursorLineNr guifg=#52B0EF guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight MatchParen guifg=#52B0EF guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight! link NormalNC Normal
+highlight Pmenu guifg=NONE guibg=#1D2025 guisp=NONE gui=NONE blend=NONE
+highlight PmenuSbar guifg=NONE guibg=#3F444A guisp=NONE gui=NONE blend=NONE
+highlight PmenuThumb guifg=NONE guibg=#72787E guisp=NONE gui=NONE blend=NONE
+highlight Question guifg=#97BE64 guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight QuickFixLine guifg=NONE guibg=#23272F guisp=NONE gui=NONE blend=NONE
+highlight SpecialKey guifg=#5A6168 guibg=NONE guisp=NONE gui=bold blend=NONE
+highlight SpellBad guifg=NONE guibg=NONE guisp=#FF6B6B gui=undercurl blend=NONE
+highlight SpellCap guifg=NONE guibg=NONE guisp=#ECBE79 gui=undercurl blend=NONE
+highlight SpellRare guifg=NONE guibg=NONE guisp=#C678DD gui=undercurl blend=NONE
+highlight Conditional guifg=#52B0EF guibg=NONE guisp=NONE gui=italic blend=NONE
+highlight Visual guifg=NONE guibg=#23272F guisp=NONE gui=NONE blend=NONE
+highlight VisualNOS guifg=#FF6B6B guibg=#23272F guisp=NONE gui=NONE blend=NONE
+highlight Whitespace guifg=#23272F guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight! link ModeMsg Normal
+highlight Include guifg=#FF6B6B guibg=NONE guisp=NONE gui=italic blend=NONE
+highlight! link MsgSeparator Normal
+highlight DiffChange guifg=NONE guibg=#36453A guisp=NONE gui=NONE blend=NONE
+highlight DiffText guifg=NONE guibg=#35453B guisp=NONE gui=underline blend=NONE
+highlight! link Float Number
+highlight! link Identifier Normal
+highlight Statement guifg=#52B0EF guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight! link Repeat Conditional
+highlight Exception guifg=#FF6B6B guibg=NONE guisp=NONE gui=italic blend=NONE
+highlight Macro guifg=#4CB5BD guibg=NONE guisp=NONE gui=italic blend=NONE
+highlight GitGutterAdd guifg=#97BE64 guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight DiffAdd guifg=NONE guibg=#36453A guisp=NONE gui=NONE blend=NONE
+highlight Typedef guifg=#ECBE79 guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight Special guifg=#DA8549 guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight SpecialChar guifg=#ECBE79 guibg=NONE guisp=NONE gui=bold blend=NONE
+highlight Comment guifg=#72787E guibg=NONE guisp=NONE gui=italic blend=NONE
+highlight GitGutterChange guifg=#ECBE79 guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight SpecialComment guifg=#C678DD guibg=NONE guisp=NONE gui=italic blend=NONE
+highlight Underlined guifg=NONE guibg=NONE guisp=NONE gui=underline blend=NONE
+highlight GitGutterDelete guifg=#FF6B6B guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight Muted guifg=#9DA1A4 guibg=NONE guisp=NONE gui=italic blend=NONE
+highlight Debug guifg=#97BE64 guibg=NONE guisp=NONE gui=italic blend=NONE
+highlight! link InfoMsg Info
+highlight PreProc guifg=#C678DD guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight! link SuccessMsg Success
+highlight GitGutterChangeDelete guifg=#DA8549 guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight! link LspDiagnosticsError Error
+highlight! link LspDiagnosticsErrorSign Error
+highlight! link LspDiagnosticsErrorFloating Error
+highlight! link LspDiagnosticsWarning Warning
+highlight! link LspDiagnosticsWarningSign Warning
+highlight! link LspDiagnosticsWarningFloating Warning
+highlight! link LspDiagnosticsInformation Info
+highlight! link LspDiagnosticsInformationSign Info
+highlight! link LspDiagnosticsInformationFloating Info
+highlight! link LspDiagnosticsHint Hint
+highlight! link LspDiagnosticsHintSign Hint
+highlight! link LspDiagnosticsHintFloating Hint
+highlight! link LspDiagnosticsUnderline Underlined
+highlight LspDiagnosticsUnderlineInfo guifg=NONE guibg=NONE guisp=#52B0EF gui=underline blend=NONE
+highlight TabLineSeparator guifg=#141414 guibg=#1D2025 guisp=NONE gui=NONE blend=NONE
+highlight! link TabLineError Error
+highlight! link TabLineWarning Warning
+highlight! link TabLineInfo Info
+highlight! link TabLineHint Hint
+highlight! link TabLineSuccess Success
+highlight TelescopePreviewBorder guifg=#F2C88C guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight TelescopePromptBorder guifg=#85ADFF guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight TelescopeResultsBorder guifg=#94E453 guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight! link TelescopeMatching IncSearch
+highlight IncSearch guifg=NONE guibg=#342E0F guisp=#ECBE79 gui=underline blend=NONE
+highlight NvimTreeIndentMarker guifg=#3F444A guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight NvimTreeSpecialFile guifg=NONE guibg=NONE guisp=NONE gui=bold blend=NONE
+highlight NvimTreeGitRenamed guifg=#DA8549 guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight NvimTreeGitMerge guifg=#C678DD guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight TelescopeSelectionCaret guifg=#C678DD guibg=#23272F guisp=NONE gui=NONE blend=NONE
+highlight TabLineSel guifg=#52B0EF guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight Directory guifg=#52B0EF guibg=NONE guisp=NONE gui=bold blend=NONE
+highlight TabLine guifg=#9DA1A4 guibg=#23272F guisp=NONE gui=NONE blend=NONE
+highlight Boolean guifg=#FDB4DB guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight LspDiagnosticsUnderlineHint guifg=NONE guibg=NONE guisp=#FDB4DB gui=underline blend=NONE
+highlight LspDiagnosticsUnderlineError guifg=NONE guibg=NONE guisp=#FF6B6B gui=underline blend=NONE
+highlight LspDiagnosticsUnderlineWarning guifg=NONE guibg=NONE guisp=#DA8549 gui=underline blend=NONE
+highlight Todo guifg=#ECBE79 guibg=NONE guisp=NONE gui=underline blend=NONE
+highlight! link MsgArea Normal
+highlight Character guifg=#FDB4DB guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight! link Label Conditional
+highlight Operator guifg=#52B0EF guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight Define guifg=#C678DD guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight Structure guifg=#52B0EF guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight! link Delimiter Normal
+highlight Italic guifg=NONE guibg=NONE guisp=NONE gui=italic blend=NONE
+highlight Type guifg=#ECBE79 guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight Tag guifg=#FDB4DB guibg=NONE guisp=NONE gui=underline blend=NONE
+highlight! link ErrorMsg Error
+highlight! link WarningMsg Warning
+highlight! link HintMsg Hint
+highlight Error guifg=#FF6B6B guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight LspReferenceRead guifg=#141414 guibg=#FF6B6B guisp=NONE gui=NONE blend=NONE
+highlight Warning guifg=#DA8549 guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight PreCondit guifg=#C678DD guibg=NONE guisp=NONE gui=bold blend=NONE
+highlight StorageClass guifg=#52B0EF guibg=NONE guisp=NONE gui=NONE blend=NONE
+highlight MoreMsg guifg=#97BE64 guibg=NONE guisp=NONE gui=NONE blend=NONE
---
+++ lua/lush_theme/glowbeam.lua
@@ -0,0 +1,311 @@
+--
+-- Built with,
+--
+--        ,gggg,
+--       d8" "8I                         ,dPYb,
+--       88  ,dP                         IP'`Yb
+--    8888888P"                          I8  8I
+--       88                              I8  8'
+--       88        gg      gg    ,g,     I8 dPgg,
+--  ,aa,_88        I8      8I   ,8'8,    I8dP" "8I
+-- dP" "88P        I8,    ,8I  ,8'  Yb   I8P    I8
+-- Yb,_,d88b,,_   ,d8b,  ,d8b,,8'_   8) ,d8     I8,
+--  "Y8P"  "Y888888P'"Y88P"`Y8P' "YY8P8P88P     `Y8
+--
+-- luacheck: read globals
+--
+
+local lush = require('lush')
+local hsl = lush.hsl
+
+local red       = hsl("#ff6c6b")
+local orange    = hsl("#da8548")
+local yellow    = hsl("#ecbe7b")
+local green     = hsl("#98be65")
+local teal      = hsl("#4db5bd")
+-- local cyan      = hsl("#46D9FF")
+local blue      = hsl("#51afef")
+local magenta   = hsl("#c678dd")
+local pink      = hsl("#fdb5db")
+-- local violet    = hsl("#a9a1e1")
+local white     = hsl("#bbc2cf")
+local black     = hsl("#141414")
+
+-- local base0     = hsl("#1b2229")
+local base1     = hsl("#1c1f24")
+-- local base2     = hsl("#202328")
+local base3     = hsl("#23272e")
+local base4     = hsl("#3f444a")
+local base5     = hsl("#5b6268")
+local base6     = hsl("#73797e")
+local base7     = hsl("#9ca0a4")
+local base8     = hsl("#b1b1b1")
+local base9     = hsl("#e6e6e6")
+
+local accent = blue
+local special = {
+	red =    hsl("#ff6c6b"),
+	yellow = hsl("#f2c68a"),
+	green =  hsl("#95e454"),
+	blue =   hsl("#87afff")
+}
+
+local groups = {
+	visual = base3,
+	search = hsl("#322c0e"),
+	fold = hsl("#f8e71c"),
+	alt = hsl("#21242b"),
+	status = base1
+}
+
+local git = {
+	add = hsl("#36453A"),
+	change = hsl("#36453A"),
+	text = hsl("#35453B"),
+	delete = hsl("#484041")
+}
+
+local diag = {
+	error = red,
+	warning = orange,
+	info = blue,
+	hint = pink,
+	success = green
+}
+
+local theme = lush(function()
+	return {
+		Normal {fg=white, bg=black}, -- normal text
+
+		Comment      {fg=base6, gui="italic"},
+		ColorColumn  {bg=base1}, -- used for the columns set with 'colorcolumn'
+		Conceal      {bg=base4}, -- placeholder characters substituted for concealed text (see 'conceallevel')
+		Cursor       {bg=special.green}, -- character under the cursor
+		lCursor      {Cursor}, -- the character under the cursor when |language-mapping| is used (see 'guicursor')
+		CursorIM     {bg=special.blue}, -- like Cursor, but used when in IME mode |CursorIM|
+		CursorLine   {bg=groups.visual}, -- Screen-line at the cursor, when 'cursorline' is set.  Low-priority if foreground (ctermfg OR guifg) is not set.
+		CursorColumn {CursorLine}, -- Screen-column at the cursor, when 'cursorcolumn' is set.
+		Directory    {fg=blue, gui="bold"}, -- directory names (and other special names in listings)
+		EndOfBuffer  {fg=base4}, -- filler lines (~) after the end of the buffer.  By default, this is highlighted like |hl-NonText|.
+		-- TermCursor   {}, -- cursor in a focused terminal
+		-- TermCursorNC {}, -- cursor in an unfocused terminal
+		VertSplit    {fg=base4}, -- the column separating vertically split windows
+		Folded       {fg=groups.fold}, -- line used for closed folds
+		FoldColumn   {fg=groups.fold}, -- 'foldcolumn'
+		SignColumn   {bg=black}, -- column where |signs| are displayed
+		IncSearch    {bg=groups.search, gui="underline", sp=yellow}, -- 'incsearch' highlighting; also used for the text replaced with ":s///c"
+		-- Substitute   {}, -- |:substitute| replacement text highlighting
+		LineNr       {fg=base4}, -- Line number for ":number" and ":#" commands, and when 'number' or 'relativenumber' option is set.
+		CursorLineNr {fg=accent}, -- Like LineNr when 'cursorline' or 'relativenumber' is set for the cursor line.
+		MatchParen   {fg=accent}, -- The character under the cursor or just before it, if it is a paired bracket, and its match. |pi_paren.txt|
+		NormalFloat  {Normal}, -- Normal text in floating windows.
+		NormalNC     {Normal}, -- normal text in non-current windows
+		Pmenu        {bg=base1}, -- Popup menu: normal item.
+		PmenuSel     {fg=base1, bg=accent}, -- Popup menu: selected item.
+		PmenuSbar    {bg=base4}, -- Popup menu: scrollbar.
+		PmenuThumb   {bg=base6}, -- Popup menu: Thumb of the scrollbar.
+		Question     {fg=green}, -- |hit-enter| prompt and yes/no questions
+		QuickFixLine {bg=groups.visual}, -- Current |quickfix| item in the quickfix window. Combined with |hl-CursorLine| when the cursor is there.
+		Search       {bg=groups.search}, -- Last search pattern highlighting (see 'hlsearch').  Also used for similar items that need to stand out.
+		SpecialKey   {fg=base5, gui="bold"}, -- Unprintable characters: text displayed differently from what it really is.  But not 'listchars' whitespace. |hl-Whitespace| SpellBad  Word that is not recognized by the spellchecker. |spell| Combined with the highlighting used otherwise.  SpellCap  Word that should start with a capital. |spell| Combined with the highlighting used otherwise.  SpellLocal  Word that is recognized by the spellchecker as one that is used in another region. |spell| Combined with the highlighting used otherwise.
+
+		SpellBad    {gui="undercurl", sp=red}, -- Word that is recognized by the spellchecker as one that is hardly ever used.  |spell| Combined with the highlighting used otherwise.
+		SpellCap    {gui="undercurl", sp=yellow}, -- Word that is recognized by the spellchecker as one that is hardly ever used.  |spell| Combined with the highlighting used otherwise.
+		SpellRare    {gui="undercurl", sp=magenta}, -- Word that is recognized by the spellchecker as one that is hardly ever used.  |spell| Combined with the highlighting used otherwise.
+
+		StatusLine   {fg=base8}, -- status line of current window
+		StatusLineNC {StatusLine, fg=base6}, -- status lines of not-current windows Note: if this is equal to "StatusLine" Vim will use "^^^" in the status line of the current window.
+
+		TabLine      {fg=base7, bg=base3}, -- tab pages line, not active tab page label
+		TabLineFill  {bg=base1}, -- tab pages line, where there are no labels
+		TabLineSel   {fg=accent}, -- tab pages line, active tab page label
+
+		Title        {fg=yellow, gui="bold"}, -- titles for output from ":set all", ":autocmd" etc.
+		Visual       {bg=groups.visual}, -- Visual mode selection
+		VisualNOS    {Visual, fg=red}, -- Visual mode selection when vim is "Not Owning the Selection".
+		Whitespace   {fg=base3}, -- "nbsp", "space", "tab" and "trail" in 'listchars'
+		NonText      {Whitespace}, -- '@' at the end of the window, characters from 'showbreak' and other characters that do not really exist in the text (e.g., ">" displayed when a double-wide character doesn't fit at the end of the line). See also |hl-EndOfBuffer|.
+		-- WildMenu     {}, -- current match in 'wildmenu' completion
+
+		MoreMsg      {fg=green}, -- |more-prompt|
+		ModeMsg      {Normal}, -- 'showmode' message (e.g., "-- INSERT -- ")
+		MsgArea      {Normal}, -- Area for messages and cmdline
+		MsgSeparator {Normal}, -- Separator for scrolled messages, `msgsep` flag of 'display'
+
+		DiffAdd      {bg=git.add}, -- diff mode: Added line |diff.txt|
+		DiffChange   {bg=git.change}, -- diff mode: Changed line |diff.txt|
+		DiffDelete   {bg=git.delete}, -- diff mode: Deleted line |diff.txt|
+		DiffText     {bg=git.text, gui="underline"}, -- diff mode: Changed text within a changed line |diff.txt|
+
+		GitGutterAdd          {fg=green},
+		GitGutterChange       {fg=yellow},
+		GitGutterDelete       {fg=red},
+		GitGutterChangeDelete {fg=orange},
+
+		-- These groups are not listed as default vim groups,
+		-- but they are defacto standard group names for syntax highlighting.
+		-- commented out groups should chain up to their "preferred" group by
+		-- default,
+		-- Uncomment and edit if you want more specific syntax highlighting.
+
+		Constant       {fg=green}, -- (preferred) any constant
+		String         {fg=green}, --   a string constant: "this is a string"
+		Number         {fg=orange}, --   a number constant: 234, 0xff
+		Float          {Number}, --    a floating point constant: 2.3e10
+		Character      {fg=pink}, --  a character constant: 'c', '\n'
+		Boolean        {fg=pink}, --  a boolean constant: TRUE, false
+
+		Identifier     {Normal}, -- (preferred) any variable name
+		Function       {fg=blue}, -- function name (also: methods for classes)
+
+		Statement      {fg=blue}, -- (preferred) any statement
+		Conditional    {Statement, gui="italic"}, --  if, then, else, endif, switch, etc.
+		Repeat         {Conditional}, --   for, do, while, etc.
+		Label          {Conditional}, --    case, default, etc.
+		Operator       {fg=blue}, -- "sizeof", "+", "*", etc.
+		Keyword        {fg=magenta, gui="italic"}, --  any other keyword
+		Exception      {fg=red, gui="italic"}, --  try, catch, throw
+
+		PreProc        {fg=magenta}, -- (preferred) generic Preprocessor
+		Include        {fg=red, gui="italic"}, --  preprocessor #include
+		Define         {fg=magenta}, --   preprocessor #define
+		Macro          {fg=teal, gui="italic"}, --    same as Define
+		PreCondit      {fg=magenta, gui="bold"}, --  preprocessor #if, #else, #endif, etc.
+
+		Type           {fg=yellow}, -- (preferred) int, long, char, etc.
+		StorageClass   {fg=blue}, -- static, register, volatile, etc.
+		Structure      {fg=blue}, --  struct, union, enum, etc.
+		Typedef        {fg=yellow}, --  A typedef
+
+		Special        {fg=orange}, -- (preferred) any special symbol
+		SpecialChar    {fg=yellow, gui="bold"}, --  special character in a constant
+		Tag            {fg=pink, gui="underline"}, --    you can use CTRL-] on this
+		Delimiter      {Normal}, --  character that needs attention
+		SpecialComment {fg=magenta, gui="italic"}, -- special things inside a comment
+
+		Underlined {gui = "underline"}, -- (preferred) text that stands out, HTML links
+		Bold       {gui = "bold"},
+		Italic     {gui = "italic"},
+
+		-- ("Ignore", below, may be invisible...)
+		-- Ignore         {}, -- (preferred) left blank, hidden  |hl-Ignore|
+
+		Error          {fg=diag.error}, -- (preferred) any erroneous construct
+		Warning        {fg=diag.warning}, -- (preferred) any warning construct
+		Info           {fg=diag.info}, -- (preferred) any info construct
+		Hint           {fg=diag.hint}, -- (preferred) any hint construct
+		Success        {fg=diag.success}, -- (preferred) any successful construct
+		Ignore         {fg=base9},
+		Muted          {fg=base7, gui="italic"},
+		Debug          {fg=green, gui="italic"}, --    debugging statements
+
+		ErrorMsg       {Error}, -- error messages on the command line
+		WarningMsg     {Warning}, -- warning messages
+		InfoMsg        {Info}, -- info messages
+		HintMsg        {Hint}, -- hint messages
+		SuccessMsg     {Success}, -- success messages
+
+		Todo           {fg=yellow, gui="underline"}, -- (preferred) anything that needs extra attention; mostly the keywords TODO FIXME and XXX
+
+		-- These groups are for the native LSP client. Some other LSP clients may use
+		-- these groups, or use their own. Consult your LSP client's documentation.
+
+		LspDiagnosticsError               {Error}, -- used for "Error" diagnostic virtual text
+		LspDiagnosticsErrorSign           {Error}, -- used for "Error" diagnostic signs in sign column
+		LspDiagnosticsErrorFloating       {Error}, -- used for "Error" diagnostic messages in the diagnostics float
+		LspDiagnosticsWarning             {Warning}, -- used for "Warning" diagnostic virtual text
+		LspDiagnosticsWarningSign         {Warning}, -- used for "Warning" diagnostic signs in sign column
+		LspDiagnosticsWarningFloating     {Warning}, -- used for "Warning" diagnostic messages in the diagnostics float
+		LspDiagnosticsInformation         {Info}, -- used for "Information" diagnostic virtual text
+		LspDiagnosticsInformationSign     {Info}, -- used for "Information" signs in sign column
+		LspDiagnosticsInformationFloating {Info}, -- used for "Information" diagnostic messages in the diagnostics float
+		LspDiagnosticsHint                {Hint}, -- used for "Hint" diagnostic virtual text
+		LspDiagnosticsHintSign            {Hint}, -- used for "Hint" diagnostic signs in sign column
+		LspDiagnosticsHintFloating        {Hint}, -- used for "Hint" diagnostic messages in the diagnostics float
+		LspReferenceText                  {fg=black, bg=red}, -- used for highlighting "text" references
+		LspReferenceRead                  {fg=black, bg=red}, -- used for highlighting "read" references
+		LspReferenceWrite                 {fg=black, bg=red}, -- used for highlighting "write" references
+
+		LspDiagnosticsUnderline           {Underlined},
+		LspDiagnosticsUnderlineError      {Underlined, sp=Error.fg},
+		LspDiagnosticsUnderlineInfo       {Underlined, sp=Info.fg},
+		LspDiagnosticsUnderlineWarning    {Underlined, sp=Warning.fg},
+		LspDiagnosticsUnderlineHint       {Underlined, sp=Hint.fg},
+
+		-- These groups are for the neovim tree-sitter highlights.
+		-- As of writing, tree-sitter support is a WIP, group names may change.
+		-- By default, most of these groups link to an appropriate Vim group,
+		-- TSError -> Error for example, so you do not have to define these unless
+		-- you explicitly want to support Treesitter's improved syntax awareness.
+
+		-- TSError              {Error}, -- For syntax/parser errors.
+		-- TSPunctDelimiter     {}, -- For delimiters ie: `.`
+		-- TSPunctBracket       {}, -- For brackets and parens.
+		-- TSPunctSpecial       {}, -- For special punctutation that does not fall in the catagories before.
+		-- TSConstant           {}, -- For constants
+		-- TSConstBuiltin       {}, -- For constant that are built in the language: `nil` in Lua.
+		-- TSConstMacro         {}, -- For constants that are defined by macros: `NULL` in C.
+		-- TSString             {}, -- For strings.
+		-- TSStringRegex        {}, -- For regexes.
+		-- TSStringEscape       {}, -- For escape characters within a string.
+		-- TSCharacter          {}, -- For characters.
+		-- TSNumber             {}, -- For integers.
+		-- TSBoolean            {}, -- For booleans.
+		-- TSFloat              {}, -- For floats.
+		-- TSFunction           {}, -- For function (calls and definitions).
+		-- TSFuncBuiltin        {}, -- For builtin functions: `table.insert` in Lua.
+		-- TSFuncMacro          {}, -- For macro defined fuctions (calls and definitions): each `macro_rules` in Rust.
+		TSParameter          {fg=teal, gui="italic"}, -- For parameters of a function.
+		-- TSParameterReference {}, -- For references to parameters of a function.
+		-- TSMethod             {}, -- For method calls and definitions.
+		-- TSField              {}, -- For fields.
+		-- TSProperty           {}, -- Same as `TSField`.
+		-- TSConstructor        {}, -- For constructor calls and definitions: `{}` in Lua, and Java constructors.
+		-- TSConditional        {}, -- For keywords related to conditionnals.
+		-- TSRepeat             {}, -- For keywords related to loops.
+		-- TSLabel              {}, -- For labels: `label:` in C and `:label:` in Lua.
+		-- TSOperator           {}, -- For any operator: `+`, but also `->` and `*` in C.
+		-- TSKeyword            {}, -- For keywords that don't fall in previous categories.
+		-- TSKeywordFunction    {}, -- For keywords used to define a fuction.
+		-- TSException          {}, -- For exception related keywords.
+		-- TSType               {}, -- For types.
+		-- TSTypeBuiltin        {}, -- For builtin types (you guessed it, right ?).
+		-- TSNamespace          {}, -- For identifiers referring to modules and namespaces.
+		-- TSInclude            {}, -- For includes: `#include` in C, `use` or `extern crate` in Rust, or `require` in Lua.
+		-- TSAnnotation         {}, -- For C++/Dart attributes, annotations that can be attached to the code to denote some kind of meta information.
+		-- TSText               {}, -- For strings considered text in a markup language.
+		-- TSStrong             {}, -- For text to be represented with strong.
+		-- TSEmphasis           {}, -- For text to be represented with emphasis.
+		-- TSUnderline          {}, -- For text to be represented with an underline.
+		-- TSTitle              {}, -- Text that is part of a title.
+		-- TSLiteral            {}, -- Literal text.
+		-- TSURI                {}, -- Any URI like a link or email.
+		-- TSVariable           {}, -- Any variable name that does not have another highlight.
+		-- TSVariableBuiltin    {}, -- Variable names that are defined by the languages, like `this` or `self`.
+
+		TabLineSeparator {fg=Normal.bg, bg=TabLineFill.bg},
+		TabLineError     {Error},
+		TabLineWarning   {Warning},
+		TabLineInfo      {Info},
+		TabLineHint      {Hint},
+		TabLineSuccess   {Success},
+
+		TelescopePreviewBorder {fg=special.yellow},
+		TelescopePromptBorder {fg=special.blue},
+		TelescopeResultsBorder {fg=special.green},
+		TelescopeSelectionCaret {Visual, fg=magenta},
+		TelescopePreviewLine {Search},
+		TelescopeMatching {IncSearch},
+
+		NvimTreeGitDirty {fg=blue},
+		NvimTreeGitStaged {fg=green},
+		NvimTreeGitMerge {fg=magenta},
+		NvimTreeGitRenamed {fg=orange},
+		NvimTreeGitNew {fg=red},
+		NvimTreeSpecialFile {gui="bold"},
+		NvimTreeFolderIcon {fg=base6},
+		NvimTreeIndentMarker {fg=base4}
+	}
+end)
+
+return theme
